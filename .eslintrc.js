85809e61701210ed52b6e59cd430d3fb6fd86664
linhuibin <1032352370@qq.com>
2022-01-03 14:04:34 +0800

    feat: 添加环境配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
+  parser: "babel-eslint",
   extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   plugins: ['prettier'],
   includes: ['src/**'],
+  settings: {
+    "import/resolver": "node"
+  }
 }
--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,14 @@
 import {defineConfig} from 'rollup';
+import resolve from 'rollup-plugin-node-resolve';
 
 export default defineConfig({
     input: 'src/index.js',
     output: {
         file: 'dist/vue.js',
-        format: 'umd',
+        format: process.env.NODE_ENV === 'production' ? 'umd' : 'esm',
         name: 'vue'
     },
+    plugins: [resolve()],
     watch: {
         chokidar: {},
         clearScreen: true,
